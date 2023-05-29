8c9a1bd44b2f93f01cecb8b065fff63def6becf6
AhmetTalhaKaya <78381990+AhmetTalhaKaya@users.noreply.github.com>
2023-05-29 09:37:00 -0500

    Añadido fecha y hora al ejecutarse

---
+++ Proy_Final_V3/db_local.cpp
@@ -0,0 +1,109 @@
+﻿#include "db_local.h"
+#include "medicion.h"
+#include <iostream>
+
+using namespace std;
+
+DB_Local::DB_Local(const string& url) : url(url), db(nullptr) {}
+
+bool DB_Local::CrearTabla(){
+    string consulta = "CREATE TABLE IF NOT EXISTS mediciones("
+                          "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                          "temperatura REAL,"
+                          "humedad REAL,"
+                          "precipitacion REAL,"
+                          "intensidad_luz REAL,"
+                          "timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)";
+
+
+        char* errMsg;
+        int resultado = sqlite3_exec(db, consulta.c_str(), nullptr, nullptr, &errMsg);
+        if (resultado != SQLITE_OK) {
+            cerr << "Error al crear la tabla: " << errMsg << endl;
+            sqlite3_free(errMsg);
+            return false;
+        }
+        return true;
+
+}
+
+bool DB_Local::Abrir_DB() {
+    int result = sqlite3_open(url.c_str(), &db);
+    if (result != SQLITE_OK) {
+        cerr << "Error al abrir la base de datos: " << sqlite3_errmsg(db) << endl;
+        return false;
+    }
+    return true;
+}
+
+bool DB_Local::Ingresar(const Medicion& med) {
+    if (!db) {
+        cerr << "Error: Base de datos no abierta." << endl;
+        return false;
+    }
+    // Como poner valores creados al VALUES ??
+    string query = "INSERT INTO mediciones(temperatura, humedad, precipitacion, intensidad_luz) VALUES ("
+                        + to_string(med.temperatura) + ", "
+                        + to_string(med.humedad) + ", "
+                        + to_string(med.precipitacion) + ", "
+                        + to_string(med.intensidadLuz) + ");";
+
+    char* errMsg;
+    int result = sqlite3_exec(db, query.c_str(), nullptr, nullptr, &errMsg);
+    if (result != SQLITE_OK) {
+        cerr << "Error al ingresar los datos en la base de datos: " << errMsg << endl;
+        sqlite3_free(errMsg);
+        return false;
+    }
+
+    return true;
+}
+
+bool DB_Local::Recuperar(Medicion& prom) {
+    if (!db) {
+        cerr << "Error: Base de datos no abierta." << endl;
+        return false;
+    }
+
+    string query = "SELECT AVG(temperatura), AVG(humedad), AVG(precipitacion), AVG(intensidad_luz) FROM mediciones;";
+
+    char* errMsg;
+    int result = sqlite3_exec(
+        db,
+        query.c_str(),
+        [](void* data, int argc, char** argv, char** /*colNames*/) {
+            if (argc == 4) {
+                Medicion* promedio = static_cast<Medicion*>(data);
+                promedio->temperatura = stof(argv[0]);
+                promedio->humedad = stof(argv[1]);
+                promedio->precipitacion = stof(argv[2]);
+                promedio->intensidadLuz = stof(argv[3]);
+            }
+            return 0;
+        },
+        &prom,
+        &errMsg);
+
+    if (result != SQLITE_OK) {
+        cerr << "Error al recuperar los datos de la base de datos: " << errMsg << endl;
+        sqlite3_free(errMsg);
+        return false;
+    }
+
+    return true;
+}
+
+bool DB_Local::Cerrar_DB() {
+    if (db) {
+        int result = sqlite3_close(db);
+        if (result != SQLITE_OK) {
+            cerr << "Error al cerrar la base de datos: " << sqlite3_errmsg(db) << endl;
+            return false;
+        }
+        db = nullptr;
+    }
+    return true;
+}
+
+
+
---
+++ Proy_Final_V3/main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include "db_local.h"
+#include "medicion.h"
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <random>
+
+using namespace std;
+
+void delay(unsigned int millisec) {
+    this_thread::sleep_for(chrono::milliseconds(millisec));
+}
+
+int main() {
+    string url = "database.db";
+    DB_Local db(url);
+
+    // Abrir base de datos
+    if (!db.Abrir_DB()) {
+        return 1;
+    }
+
+    // Crear la tabla "mediciones"
+    if (!db.CrearTabla()) {
+        db.Cerrar_DB();
+        return 1;
+    }
+
+    time_t tiempoAhora = time(nullptr);
+
+        tm* fechaHora = localtime(&tiempoAhora);
+
+        // Extrae los componentes de fecha y hora
+        int ano = fechaHora->tm_year + 1900;
+        int mes = fechaHora->tm_mon + 1;
+        int dia = fechaHora->tm_mday;
+        int hora = fechaHora->tm_hour;
+        int minuto = fechaHora->tm_min;
+        int segundo = fechaHora->tm_sec;
+
+        // Muestra la fecha y hora de ejecución
+        cout << "Fecha y hora de ejecución: " << dia << "/" << mes << "/" << ano
+             << " " << hora << ":" << minuto << ":" << segundo << endl;
+
+    vector<Medicion> mediciones;
+    double sumaTemperatura = 0.0;
+    double sumaHumedad = 0.0;
+    double sumaPrecipitacion = 0.0;
+    double sumaIntensidadLuz = 0.0;
+    int contador = 0;
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<double> tempDist(-10.0, 45.0);
+    uniform_real_distribution<double> humDist(0.0, 100.0);
+    uniform_real_distribution<double> precDist(0.0, 50.0);
+    uniform_real_distribution<double> luzDist(0.0, 2000.0);
+
+
+    while (true) {
+        delay(5000);
+
+        Medicion medicion;
+        // Generar valores aleatorios para las mediciones
+        medicion.temperatura   = tempDist(gen);
+        medicion.humedad       = humDist(gen);
+        medicion.precipitacion = precDist(gen);
+        medicion.intensidadLuz = luzDist(gen);
+
+        // Insertar la medición en la base de datos
+        if (db.Ingresar(medicion)) {
+            cout << "Datos ingresados en la base de datos." << endl;
+        } else {
+            cerr << "Error al ingresar los datos." << endl;
+        }
+
+
+        sumaTemperatura += medicion.temperatura;
+        sumaHumedad += medicion.humedad;
+        sumaPrecipitacion += medicion.precipitacion;
+        sumaIntensidadLuz += medicion.intensidadLuz;
+        contador++;
+
+        cout << "Temperatura: " << medicion.temperatura << "°C" <<endl;
+        cout << "Humedad: " << medicion.humedad << "%" << endl;
+        cout << "Precipitacion: " << medicion.precipitacion << " mm" << endl;
+        cout << "Intensidad de Luz: " << medicion.intensidadLuz << " lumen" << endl;
+
+        if (contador == 12) { // No olvida cambiar a 12 despues -
+            double promedioTemperatura = sumaTemperatura / contador;
+            double promedioHumedad = sumaHumedad / contador;
+            double promedioPrecipitacion = sumaPrecipitacion / contador;
+            double promedioIntensidadLuz = sumaIntensidadLuz / contador;
+
+            Medicion promedio;
+            promedio.temperatura = promedioTemperatura;
+            promedio.humedad = promedioHumedad;
+            promedio.precipitacion = promedioPrecipitacion;
+            promedio.intensidadLuz = promedioIntensidadLuz;
+
+            cout << "Promedio de temperatura: " << promedio.temperatura << "°C" <<endl;
+            cout << "Promedio de humedad: " << promedio.humedad << "%" <<endl;
+            cout << "Promedio de precipitación: " << promedio.precipitacion << " mm" << endl;
+            cout << "Promedio de intensidad de luz: " << promedio.intensidadLuz << " lumen" <<  endl;
+
+            sumaTemperatura = 0.0;
+            sumaHumedad = 0.0;
+            sumaPrecipitacion = 0.0;
+            sumaIntensidadLuz = 0.0;
+            contador = 0;
+        }
+    }
+
+    db.Cerrar_DB();
+
+    return 0;
+}
